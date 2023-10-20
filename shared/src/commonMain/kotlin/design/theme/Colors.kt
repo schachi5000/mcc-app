a4f8ed8a2f58737ac3588ad6d3209d7498db33aa
schachi5000 <alexander.schacher@graphmasters.net>
2023-10-20 14:59:19 +0200

    :art: updated colors

--- shared/src/commonMain/kotlin/design/theme/Colors.kt
+++ shared/src/commonMain/kotlin/design/theme/Colors.kt
@@ -2,8 +2,15 @@ package design.theme
 
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
 
-val LightColorScheme = lightColors()
+val LightColorScheme = lightColors(
+    background = Color(0xFFF0F0F0),
+    surface = Color(0xFFFFFFFF),
+)
 
-val DarkColorScheme = darkColors()
+val DarkColorScheme = darkColors(
+    background = Color(0xFF000000),
+    surface = Color(0xFF121212),
+)
 
