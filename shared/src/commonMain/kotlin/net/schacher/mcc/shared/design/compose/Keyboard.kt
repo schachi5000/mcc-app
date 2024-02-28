7b2cc29ea68f0e941f5d3a416aa95320dfcc825e
schachi5000 <alexander.schacher@graphmasters.net>
2024-02-28 15:00:28 +0100

    :bug: increased the min inset to determine the soft keyboard to be showing

--- shared/src/commonMain/kotlin/net/schacher/mcc/shared/design/compose/Keyboard.kt
+++ shared/src/commonMain/kotlin/net/schacher/mcc/shared/design/compose/Keyboard.kt
@@ -5,5 +5,10 @@ import androidx.compose.foundation.layout.ime
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 
+/**
+ * Returns true if the keyboard is visible, false otherwise.
+ * The bottom inset should be greater than 10 to be considered visible.
+ * 0 used to work but it's not reliable anymore since the compose multiplatform 1.6.0 release.
+ **/
 @Composable
-fun isKeyboardVisible(): Boolean = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+fun isKeyboardVisible(): Boolean = WindowInsets.ime.getBottom(LocalDensity.current) > 10
