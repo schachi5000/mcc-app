46388d70b6e936973ed49e74b02e544e6c97c7bf
alexander <alex.schacher@gmail.com>
2024-04-15 16:25:05 +0200

    :bug: #50 added dedicated AppScreen with ViewModel

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,10 +1,10 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import co.touchlab.kermit.Logger
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import net.schacher.mcc.shared.auth.AuthHandler
 import net.schacher.mcc.shared.auth.PersistingAuthHandler
 import net.schacher.mcc.shared.datasource.database.CardDatabaseDao
@@ -30,6 +30,7 @@ import net.schacher.mcc.shared.screens.settings.SettingsViewModel
 import net.schacher.mcc.shared.screens.spotlight.SpotlightViewModel
 import net.schacher.mcc.shared.utils.debug
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 import org.koin.core.KoinApplication
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -45,6 +46,7 @@ val repositories = module {
 }
 
 val viewModels = module {
+    singleOf(::AppViewModel)
     singleOf(::MainViewModel)
     singleOf(::MyDecksViewModel)
     singleOf(::NewDeckViewModel)
@@ -56,41 +58,57 @@ val viewModels = module {
 
 @Composable
 fun App(
-    databaseDao: DatabaseDao,
-    onKoinStart: KoinApplication.() -> Unit = {}
+    databaseDao: DatabaseDao, onKoinStart: KoinApplication.() -> Unit = {}
 ) {
     val authHandler = PersistingAuthHandler(databaseDao as SettingsDao)
-    KoinApplication(
-        application = {
-            onKoinStart()
-            modules(
-                platformModule,
-                module {
-                    single<CardDatabaseDao> { databaseDao }
-                    single<DeckDatabaseDao> { databaseDao }
-                    single<PackDatabaseDao> { databaseDao }
-                    single<SettingsDao> { databaseDao }
-                },
-                module {
-                    single<AuthHandler> { authHandler }
-                },
-                network,
-                repositories,
-                viewModels
-            )
-        }) {
+    KoinApplication(application = {
+        onKoinStart()
+        modules(platformModule, module {
+            single<CardDatabaseDao> { databaseDao }
+            single<DeckDatabaseDao> { databaseDao }
+            single<PackDatabaseDao> { databaseDao }
+            single<SettingsDao> { databaseDao }
+        }, module {
+            single<AuthHandler> { authHandler }
+        }, network, repositories, viewModels
+        )
+    }) {
         MccTheme {
-            val loggedIn = authHandler.loginState.collectAsState()
-            var guessedLogin by remember { mutableStateOf(false) }
+            AppScreen()
+        }
+    }
+}
 
-            Logger.debug { "loggedIn: $loggedIn guessedLogin: $guessedLogin" }
-            if (loggedIn.value || guessedLogin) {
-                MainScreen()
-            } else {
-                LoginScreen {
-                    guessedLogin = true
-                }
+@Composable
+fun AppScreen(appViewModel: AppViewModel = koinInject()) {
+    val loggedIn = appViewModel.state.collectAsState()
+    if (loggedIn.value) {
+        MainScreen()
+    } else {
+        LoginScreen {
+            appViewModel.onGuestLoginClicked()
+        }
+    }
+}
+
+class AppViewModel(
+    private val authHandler: AuthHandler
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(authHandler.isLoggedIn())
+
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            authHandler.loginState.collect {
+                Logger.debug { "Login state changed: $it" }
+                _state.value = it
             }
         }
+    }
+
+    fun onGuestLoginClicked() {
+        _state.value = true
     }
 }
